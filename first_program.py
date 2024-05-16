86da9d318d6b96f2b5f33c826d99eef98595777f
Garry <71922886+voidborne@users.noreply.github.com>
2024-05-16 13:02:52 -0500

    Copied first_program.py file to github repository and set up folder on local drive.

---
+++ first_program.py
@@ -0,0 +1,49 @@
+'''
+Garry Scott
+05/15/2024
+First Program
+'''
+
+print('Hello World')
+
+print('''She said "It's Wednesday".''')
+
+'''
+Always start a variable with a letter, not a number. 
+No special charactors.
+
+'''
+
+my_name = 'Garry Scott'
+print(my_name)
+
+a = 15
+b = 5
+c = a - b
+
+print('a')
+print(a)
+print(c)
+print(c*5)
+
+x = 5
+y = 3 * x
+x = y - 5
+y = 2 * x
+
+print(x+y)
+
+test = 45%3.67
+print(test)
+
+a = 20
+b = 30
+c = b / a
+d = b // a
+print(c)
+print(d)
+print(c+d)
+
+a = 80
+b = 90
+print(a%b)
