780b464c2164ec0a782be43cb50eaa073844b09d
Garry <71922886+voidborne@users.noreply.github.com>
2024-05-20 13:25:47 -0500

    Changes

---
+++ Module 1/Programs/grade_calculator.py
@@ -0,0 +1,70 @@
+'''
+Sudo Code
+
+Assignments
+
+Total points are 102%, 100% + 2%
+
+Quizzes 8x: 9 total 1 lowest dropped 16% | 2% each
+LABS 8x: 9 Total 1 Lowest Dropped 16% | 2 % each
+Exam 1 1x: 20%
+Exam 2 1x: 20%
+Projects 3x: 18% | 6% each
+Final Project 1x: 10%
+Extra Credit: 2% max
+
+
+Grading Scale
+
+92.50 - 102.00  |   A   |   4.00
+89.50 - 92.40   |   A-  |   3.67
+86.50 - 89.40   |   B+  |   3.33
+82.50 - 86.40   |   B   |   3.00    
+79.50 - 82.40   |   B-  |   2.67
+76.50 - 79.40   |   C+  |   2.33
+72.50 - 76.40   |   C   |   2.00
+69.50 - 72.40   |   C-  |   1.67
+66.50 - 69.40   |   D+  |   1.33
+62.50 - 66.40   |   D   |   1.00
+59.50 - 62.40   |   D-  |   0.67
+00.00 - 59.90   |   E   |   0.00
+
+
+Inputs
+    Scores from assignments
+
+Calculations
+    Total Points possible / points acheived
+
+Outputs
+    Letter Grade
+    Grade Points
+    Percent
+
+UI
+
+Grade Input
+    Dropdown for Assignment |   Float Grade Input | Enter Button
+
+Grid 
+    Assignment_name |   Points_Possible |   Input_Points_Achieved
+
+Info Box
+    Percentage Points
+    Grade Points
+    Course Grade
+
+Functions
+    Input_Grade_Function
+
+'''
+import tkinter as tk
+
+root = tk.Tk()
+
+root.title('Grade Calculator')
+root.eval('tk::PlaceWindow . center')
+root.geometry('880x440')
+root.configure(bg='#333333')
+
+root.mainloop()
