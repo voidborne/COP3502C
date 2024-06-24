5d464d44c119c0edd0dcfc312f41f964db40fa4e
Garry <71922886+voidborne@users.noreply.github.com>
2024-06-24 03:53:47 -0500

    commit

--- PROGRAMS/Practice_Programs/module_2_practice.py
+++ PROGRAMS/Practice_Programs/module_2_practice.py
@@ -4,4 +4,34 @@
 
 #Q3.) C
 
-#Q4.) 
+#Q4.) C
+
+#Q5.) C
+
+#Q6.) B
+
+#P1
+
+def prompt_user_for_input():
+    mass = float(input('The mass is: '))
+    velocity = float(input('The velocity is: '))
+    return mass, velocity
+
+def kinetic_energy_caculator(mass, velocity):
+    kinetic_energy = 0.5*mass*velocity**2
+    return round(kinetic_energy, 2)
+
+def main():
+    mass, velocity = prompt_user_for_input()
+    kinetic_energy = kinetic_energy_caculator(mass, velocity)
+    print(f'The kinetic energy is: {kinetic_energy}')
+
+if __name__=='__main__':
+    main()
+
+
+#P2
+
+
+
+
