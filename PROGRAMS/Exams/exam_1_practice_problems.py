e521b0e791039ac2a24c2213e3cda58d9b98e5ea
Garry <71922886+voidborne@users.noreply.github.com>
2024-06-24 03:09:27 -0500

    commit

--- PROGRAMS/Exams/exam_1_practice_problems.py
+++ PROGRAMS/Exams/exam_1_practice_problems.py
@@ -51,14 +51,50 @@ if __name__=='__main__':
     main()
 '''
 
+'''
 def min_terms(n):
-    k = 1
     _sum = 0
-    while _sum < n:
+    k = 1
+    while True:
         _sum += (k**k)/(2*k)
-        if _sum >= n:
+        if _sum > n:
             return k
         k += 1
 
 k = min_terms(10000)
 print(k)
+
+'''
+
+'''
+# /3 = fizz, /5 = buzz, / 3 and 5 = fizzbuzz
+
+def fizzbuzz(_max):
+    for i in range(1, _max+1):
+        if i % 3 == 0 and i % 5 == 0:
+            print('fizzbuzz')
+        elif i % 3 == 0:
+            print('fizz')
+        elif i % 5 == 0:
+            print('buzz')
+        else:
+            print(i)
+
+fizzbuzz(15)
+'''
+'''
+def print_pyramid(height):
+    for n in range(1, height + 1):
+        spaces = ' ' * (height - n)
+        stars = '*' * (2 * n - 1)
+        print(spaces + stars)
+
+print_pyramid(5)
+'''
+my_dict = {'apple': 3, 'banana': 5, 'orange': 2}
+
+x = my_dict['banana']
+y = my_dict.get('grape', 5)
+z = len(my_dict)
+
+print(x + y + z)
